3c3ac6aca000cd967b28bd61e4909f9cc2bc7552
yokoto <youkotoushi@gmail.com>
2021-06-27 12:15:36 +0900

    add fetch to get data and veux

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -11,7 +11,7 @@
 </template>
 
 <script>
-import PostList from '@/components/Posts/Postlist'
+import PostList from '@/components/Posts/PostList'
 import AppButton from '@/components/UI/AppButton'
 
 export default {
