a9f00f551e5416ae2013513801176e6e527958d4
yokoto <youkotoushi@gmail.com>
2021-07-01 00:06:40 +0900

    add config, plugins and modules

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,14 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'my-second-nuxt-app',
+    title: 'my-first-nuxt-app',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'My Cool Web Development Blog' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
@@ -23,10 +23,13 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '~assets/styles/main.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '~plugins/core-components.js',
+    '~plugins/date-filter.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -38,9 +41,34 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxtjs/axios',
   ],
+  axios: {
+    baseURL: process.env.BASE_URL || 'https://nuxt-blog-ed024-default-rtdb.firebaseio.com',
+    credentials: false
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+
+  // Customize the progress-bar color
+  loading: {
+    color: '#fa923f',
+    height: '4px',
+    duration: 5000
+  },
+
+  loadingIndicator: {
+    name: 'circle',
+    color: '#fa923f'
+  },
+
+  env: {
+    baseUrl: process.env.BASE_URL || 'https://nuxt-blog-ed024-default-rtdb.firebaseio.com'
+  },
+  transition: {
+    name: 'fade',
+    mode: 'out-in'
   }
 }
--- store/index.js
+++ store/index.js
@@ -1,5 +1,4 @@
 import Vuex from 'vuex'
-import axios from 'axios'
 
 const createStore = () => {
   return new Vuex.Store({
@@ -22,11 +21,12 @@ const createStore = () => {
     },
     actions: {
       nuxtServerInit(vuexContext, context) {
-        return axios.get('https://nuxt-blog-ed024-default-rtdb.firebaseio.com/posts.json')
-          .then(res => {
+        return context.app.$axios
+          .$get('/posts.json')
+          .then(data => {
             const postsArray = []
-            for (const key in res.data) {
-              postsArray.push({ ...res.data[key], id: key })
+            for (const key in data) {
+              postsArray.push({ ...data[key], id: key })
             }
             vuexContext.commit('setPosts', postsArray)
           })
@@ -37,19 +37,20 @@ const createStore = () => {
           ...post,
           updatedDate: new Date()
         }
-        axios
-          .post('https://nuxt-blog-ed024-default-rtdb.firebaseio.com/posts.json', createdPost)
-          .then(result => {
-            vuexContext.commit('addPost', { ...createdPost, id: result.data.name })
+        return this.$axios
+          .$post('/posts.json', createdPost)
+          .then(data => {
+            vuexContext.commit('addPost', { ...createdPost, id: data.name })
           })
           .catch(e => console.log(e))
       },
       editPost(vuexContext, editedPost) {
-        return axios.put('https://nuxt-blog-ed024-default-rtdb.firebaseio.com/posts/' + 
-          editedPost.id +
-          '.json', editedPost)
-          .then(res => {
-            vuexContext.commit('editPost', editedPost)
+        return this.$axios
+          .$put('/posts/' + 
+            editedPost.id +
+            '.json', editedPost)
+            .then(data => {
+              vuexContext.commit('editPost', editedPost)
           })
           .catch(e => console.log(e))
       },
